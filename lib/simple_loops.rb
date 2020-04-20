a85501fd41b310be420c7d40769a76bdb9edaa19
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-04-20 13:18:45 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,24 @@
-# Write your methods here
+def loop_message_five_times(message)
+    count = 0
+  while count < 5 do
+    puts message
+    count += 1
+  end
+end
+
+def loop_message_n_times(message, integer)
+    count = 0
+  while count < integer do
+    puts message
+    count += 1
+  end
+end
+
+def output_array(array)
+    count = 0
+  while array 
+end
+
+def return_string_array
+  
+end
